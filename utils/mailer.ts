47ba4359db4742292b25fb5cd7ce8fec6ec54343
Harsh Dagar <amandagar000@gmail.com>
2024-07-24 00:51:31 +0530

    use hd-mailer

--- utils/mailer.ts
+++ utils/mailer.ts
@@ -1,39 +1,23 @@
-import nodemailer from "nodemailer";
-
-const mailTransporter = nodemailer.createTransport({
-  host: "smtpout.secureserver.net",
-  port: 465,
-  secure: true,
-  auth: {
-    user: process.env.EMAIL_USERNAME,
-    pass: process.env.EMAIL_PASSWORD,
-  },
-});
-
 /**
  *
  * @param param.to Email address of recipient
  * @param param.subject Subject line of the Email
  * @param param.html HTML body of the email
- * @param param.sender Sender info @default sender Chit-Chat <${process.env.EMAIL_USERNAME}>
  * @returns
  */
-const sendEmail = ({
-  to,
-  subject,
-  sender = `Chit-Chat <${process.env.EMAIL_USERNAME}>`,
-  html,
-}: {
-  to: string;
-  subject: string;
-  sender?: string;
-  html: string;
-}) => {
-  return mailTransporter.sendMail({
-    from: sender,
-    to,
-    subject,
-    html,
+const sendEmail = ({ to, subject, html }: { to: string; subject: string; sender?: string; html: string }) => {
+  return fetch(process.env.EMAIL_SERVICE_URL as string, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.EMAIL_SERVICE_TOKEN}`,
+    },
+    body: JSON.stringify({
+      fromService: "Chit-Chat",
+      toEmail: to,
+      subject,
+      html,
+    }),
   });
 };
 
